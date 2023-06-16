582f6da011714d3262a6afa5dc0c421a856d243f
Juan David Osorio <dipeixx@gmail.com>
2023-06-16 05:22:52 -0500

    add sonar and fix

--- src/main/java/com/example/demo/controllers/CalculoController.java
+++ src/main/java/com/example/demo/controllers/CalculoController.java
@@ -1,11 +1,6 @@
 package com.example.demo.controllers;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
