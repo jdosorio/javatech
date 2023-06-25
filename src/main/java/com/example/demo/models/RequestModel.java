fec3c5de192c3602f6c429943b2e20e77df90ed0
Juan David Osorio <dipeixx@gmail.com>
2023-06-24 19:02:58 -0500

    se adiciona lombok

--- src/main/java/com/example/demo/models/RequestModel.java
+++ src/main/java/com/example/demo/models/RequestModel.java
@@ -4,9 +4,12 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import lombok.Data;
+
 //Modelo que se encarga de definir los 3 parametros necesarios para realizar
 // el calculo solicitado. Implementa sus getters y setters para tener acceso
 // a sus variables o atributos
+@Data
 public class RequestModel {
 
     @NotNull(message = "El parametro N es obligatorio")
@@ -18,28 +21,4 @@ public class RequestModel {
 
     @NotNull(message = "El parametro Y es obligatorio")
     private Integer paramY;
-
-    public void setParamN(Integer paramN) {
-        this.paramN = paramN;
-    }
-
-    public void setParamX(Integer paramX) {
-        this.paramX = paramX;
-    }
-
-    public void setParamY(Integer paramY) {
-        this.paramY = paramY;
-    }
-
-    public Integer getParamN() {
-        return paramN;
-    }
-
-    public Integer getParamX() {
-        return paramX;
-    }
-
-    public Integer getParamY() {
-        return paramY;
-    }
 }
--- src/main/java/com/example/demo/models/ResponseModel.java
+++ src/main/java/com/example/demo/models/ResponseModel.java
@@ -2,36 +2,15 @@ package com.example.demo.models;
 
 import java.util.List;
 
+import lombok.Data;
+
 //Modelo que se encarga de definir la respuesta o retorno del metodo calculo.
 // Implementa sus getters y setters para tener acceso
 // a sus variables o atributos
-
+@Data
 public class ResponseModel {
     private Integer data;
     private Integer status;
     private List<String> errors;
 
-    public void setErrors(List<String> errors) {
-        this.errors = errors;
-    }
-
-    public void setData(Integer data) {
-        this.data = data;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public Integer getData() {
-        return data;
-    }
-
-    public List<String> getErrors() {
-        return errors;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
 }
